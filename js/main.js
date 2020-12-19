4976f91dc4b2d3f2912ba23dc13479381ed26a8d
Sumit Yadav <ysumit99@gmail.com>
2020-12-20 04:20:13 +0530

    Board Setup

---
+++ js/main.js
@@ -0,0 +1,71 @@
+const colorCodes = [
+    {
+        value: 2,
+        color: "blue",
+        backgroundColor: "red"
+
+    },
+    {
+        value: 4,
+        color: "blue",
+        backgroundColor: "red"
+
+    },
+    {
+        value: 8,
+        color: "blue",
+        backgroundColor: "red"
+
+    },
+    {
+        value: 16,
+        color: "blue",
+        backgroundColor: "red"
+
+    },
+    {
+        value: 32,
+        color: "blue",
+        backgroundColor: "red"
+
+    },
+    {
+        value: 64,
+        color: "blue",
+        backgroundColor: "red"
+
+    },
+    {
+        value: 128,
+        color: "blue",
+        backgroundColor: "red"
+
+    },
+    {
+        value: 256,
+        color: "blue",
+        backgroundColor: "red"
+
+    },
+    {
+        value: 256,
+        color: "blue",
+        backgroundColor: "red"
+
+    },
+    {
+        value: 1024,
+        color: "blue",
+        backgroundColor: "red"
+
+    },
+    {
+        value: 2048,
+        color: "blue",
+        backgroundColor: "red"
+
+    }
+
+];
+
+console.log(colorCodes);
